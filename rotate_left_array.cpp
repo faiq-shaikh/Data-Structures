96ac0916035e3b5d56e8c7972783201e19f5e3a5
Muhammad Faiq Shaikh <faiqshaikhm@gmail.com>
2026-07-16 17:06:28 +0500

    Add files via upload

---
+++ rotate_left_array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,temp;
+    cout<<"enter size of array: ";
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout<<"enter "<<i<<" value: ";
+        cin>>arr[i];
+    }
+    temp=arr[0];
+    for (int  i = 0; i < n-1; i++)
+    {
+        arr[i]=arr[i+1];
+    }
+    arr[n-1]=temp;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<" "<<arr[i];
+    }
+    
+}
